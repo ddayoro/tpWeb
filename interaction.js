cae8a204f31389622110eb5246937643dc7cc9d9
Dackory DAYORO <dackory.dayoro@etudiant.univ-rennes1.fr>
2021-01-14 13:31:25 +0100

    Implémentation de l'interaction: Drag-n-Drop

--- interaction.js
+++ interaction.js
@@ -1,12 +1,53 @@
 
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
+
+
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+
+  // Définir ici les attributs de la 'classe'
+  this.xInit = 0 ;
+  this.yInit = 0 ;
+  this.xFinal = 0 ;
+  this.yFinal = 0 ;
+
+  this.pression = false ;
 
 	// Developper les 3 fonctions gérant les événements
+  fonctionPression = function (evt) {
+    if(! (this.pression)) {
+      position = getMousePosition(canvas, evt) ;
+      this.xInit = position.x ;
+      this.yInit = position.y ;
+      this.pression = true ;
+      console.log("Pression : ( "+this.xInit +" , "+this.yInit+" )")
+    }
+  }.bind(this) ;
+  
+  fonctionDeplacement = function (evt) {
+    if (this.pression){
+      position = getMousePosition(canvas, evt) ;
+      this.xFinal = position.x ;
+      this.xFinal = position.y ;
+      console.log("Deplacement : ( "+this.xFinal +" , "+this.yFinal+" )")
+    }
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+  }.bind(this) ;
+
+  fonctionRelachement = function (evt) {
+    if (this.pression) {
+      position = getMousePosition(canvas, evt) ;
+      this.xFinal = position.x ;
+      this.yFinal = position.y ;
+      this.pression = false ;
+      console.log("Relachement : ( "+this.xFinal +" , "+this.yFinal+" )")
+    }
+  }.bind(this);
+
+  // Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener("mousedown", fonctionPression) ;
+  canvas.addEventListener("mousemove", fonctionDeplacement) ;
+  canvas.addEventListener("mouseup", fonctionRelachement) ;
 };
 
 
