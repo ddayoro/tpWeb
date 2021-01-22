e64553160997f6920e95e4d644d5498c7abfee47
ddayoro <dackory.dayoro@etudiant.univ-rennes1.fr>
2021-01-22 15:44:31 +0100

    Derniere modification

--- controller.js
+++ controller.js
@@ -1,17 +1,54 @@
-
-var editingMode = { rect: 0, line: 1 };
+var editingMode = {rect: 0, line: 1, cercle: 2};
 
 function Pencil(ctx, drawing, canvas) {
-	this.currEditingMode = editingMode.line;
-	this.currLineWidth = 5;
-	this.currColour = '#000000';
-	this.currentShape = 0;
+    this.currEditingMode = editingMode.cercle;
+    this.currLineWidth = 5;
+    this.currColour = '#000000';
+    this.currentShape = 0;
 
-	// Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
+    // Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
+    new DnD(canvas, this);
 
-	new DnD(canvas, this);
+    // Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
+    this.onInteractionStart = function (dnd) {
+        this.currLineWidth = document.getElementById("spinnerWidth").value;
+        this.currColour = document.getElementById("colour").value;
+    };
 
-	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
-};
+    this.onInteractionUpdate = function (dnd) {
+		console.log(this.currEditingMode) 
+        switch (this.currEditingMode) {
+            case editingMode.rect: {
+				console.log("Vous tracez un rectangle") ;
+                this.currentShape = new Rectangle(dnd.initX, dnd.initY, dnd.finalX - dnd.initX, dnd.finalY - dnd.initY, this.currLineWidth, this.currColour, this.currentStyle);
+                drawing.paint(ctx, canvas);
+                this.currentShape.paint(ctx, canvas);
+                break;
+            }
+            case editingMode.line: {
+				console.log("Vous tracez une ligne ") ;
+                this.currentShape = new Line(dnd.initX, dnd.initY, dnd.finalX, dnd.finalY, this.currLineWidth, this.currColour, this.currentStyle);
+                drawing.paint(ctx, canvas);
+                this.currentShape.paint(ctx, canvas);
+                break;
+			}
+			case editingMode.cercle: {
+				console.log("Vous tracez un cercle") ;
+                var rayon = Math.abs((dnd.finalX - dnd.initX) / 2);
+                if (rayon < Math.abs(((dnd.finalY - dnd.initY) / 2))) {
+                    rayon = Math.abs((dnd.finalY - dnd.initY) / 2);
+                }
+                this.currentShape = new Cercle(dnd.initX + (dnd.finalX - dnd.initX) / 2, dnd.initY + (dnd.finalY - dnd.initY) / 2, rayon, this.currLineWidth, this.currColour );
+                drawing.paint(ctx, canvas);
+                this.currentShape.paint(ctx, canvas);
+                break;
+            }
+        }
+    };
 
-
+    this.onInteractionEnd = function (dnd) {
+        console.log("Dessin termineé");
+        drawing.addFormes(this.currentShape);
+        drawing.paint(ctx, canvas);
+    };
+}
--- interaction.js
+++ interaction.js
@@ -1,64 +1,56 @@
-
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 
-
 function DnD(canvas, interactor) {
-
   // Définir ici les attributs de la 'classe'
-  this.xInit = 0 ;
-  this.yInit = 0 ;
-  this.xFinal = 0 ;
-  this.yFinal = 0 ;
+  this.initX = 0;
+  this.initY = 0;
+  this.finalX = 0;
+  this.finalY = 0;
+  this.pression = false;
 
-  this.pression = false ;
+  // Developper les 3 fonctions gérant les événements
+  this.fonctionPression = function (evt) {
+      var pos = getMousePosition(canvas, evt);
+      this.initX = pos.x;
+      this.initY = pos.y;
+      this.pression = true;
+      interactor.onInteractionStart(this);
+      console.log("Pression sur la souris : ( "+this.initX +" , "+this.initY+" )")
+  }.bind(this);
 
-	// Developper les 3 fonctions gérant les événements
-  fonctionPression = function (evt) {
-    if(! (this.pression)) {
-      position = getMousePosition(canvas, evt) ;
-      this.xInit = position.x ;
-      this.yInit = position.y ;
-      this.pression = true ;
-      console.log("Pression : ( "+this.xInit +" , "+this.yInit+" )")
-    }
-  }.bind(this) ;
-  
-  fonctionDeplacement = function (evt) {
-    if (this.pression){
-      position = getMousePosition(canvas, evt) ;
-      this.xFinal = position.x ;
-      this.xFinal = position.y ;
-      console.log("Deplacement : ( "+this.xFinal +" , "+this.yFinal+" )")
-    }
+  this.fonctionDeplacement = function (evt) {
+      if (this.pression){
+          var pos = getMousePosition(canvas, evt);
+          this.finalX = pos.x;
+          this.finalY = pos.y;
+          interactor.onInteractionUpdate(this);
+          console.log("Deplacement de la souris");
+      }
+  }.bind(this);
 
-  }.bind(this) ;
-
-  fonctionRelachement = function (evt) {
-    if (this.pression) {
-      position = getMousePosition(canvas, evt) ;
-      this.xFinal = position.x ;
-      this.yFinal = position.y ;
-      this.pression = false ;
-      console.log("Relachement : ( "+this.xFinal +" , "+this.yFinal+" )")
-    }
+  this.fonctionRelachement = function (evt) {
+      if (this.pression){
+          var pos = getMousePosition(canvas, evt);
+          this.finalX = pos.x;
+          this.finalY = pos.y;
+          this.pression = false;
+          this.moving = false;
+          interactor.onInteractionEnd(this);
+          console.log("Relachement de la souris : ( "+this.finalX +" , "+this.finalY+" )") ;
+      }
   }.bind(this);
 
   // Associer les fonctions précédentes aux évènements du canvas.
-  canvas.addEventListener("mousedown", fonctionPression) ;
-  canvas.addEventListener("mousemove", fonctionDeplacement) ;
-  canvas.addEventListener("mouseup", fonctionRelachement) ;
-};
-
-
-// Place le point de l'événement evt relativement à la position du canvas.
+  canvas.addEventListener('mousedown', this.fonctionPression, false);
+  canvas.addEventListener('mousemove', this.fonctionDeplacement, false);
+  canvas.addEventListener('mouseup', this.fonctionRelachement, false);
+}
+// Place le point de l'événement event relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
+      x: Math.round(evt.clientX - rect.left),
+      y: Math.round(evt.clientY - rect.top)
   };
-};
-
-
-
+}
