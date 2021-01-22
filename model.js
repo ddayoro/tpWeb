44c43e66056af06a4d7d3a20b1034b9c5d7967db
ddayoro <dackory.dayoro@etudiant.univ-rennes1.fr>
2021-01-22 15:49:09 +0100

    Ajout du model et view

--- model.js
+++ model.js
@@ -1,3 +1,97 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing() {
+    this.formes = new Array ();
+
+    this.getFormes = function () {
+        return this.formes;
+    }.bind(this);
+
+    this.addFormes = function(forme) {
+        this.formes.push(forme);
+    }.bind(this) ;
+};
+
+function Forme(epaisseur, couleur) {
+    this.epaisseur = epaisseur;
+    this.couleur = couleur;
+
+    this.getEpaisseur = function () {
+        return this.epaisseur;
+    }.bind(this);
+
+    this.getCouleur = function () {
+        return this.couleur;
+    }.bind(this);
+};
+
+function Rectangle(x, y, largeur, hauteur, epaisseur, couleur ) {
+    Forme.call(this, epaisseur, couleur);
+    this.initX = x;
+    this.initY = y;
+    this.largeur = largeur;
+    this.hauteur = hauteur;
+
+    this.getInitX = function () {
+        return this.initX;
+    }.bind(this);
+
+    this.getInitY = function () {
+        return this.initY;
+    }.bind(this);
+
+    this.getLargeur = function () {
+        return this.largeur;
+    }.bind(this);
+
+    this.getHauteur = function () {
+        return this.hauteur;
+    }.bind(this);
+};
+Rectangle.prototype = new Forme();
+
+function Line(x1, y1, x2, y2, epaisseur, couleur ) {
+    Forme.call(this, epaisseur, couleur);
+    this.initX = x1;
+    this.initY = y1;
+    this.finalX = x2;
+    this.finalY = y2;
+
+    this.getInitX = function () {
+        return this.initX;
+    }.bind(this);
+
+    this.getInitY = function () {
+        return this.initY;
+    }.bind(this);
+
+    this.getFinalX = function () {
+        return this.finalX;
+    }.bind(this);
+
+    this.getFinalY = function () {
+        return this.finalY;
+    }.bind(this);
+};
+Line.prototype = new Forme();
+function Cercle (x, y, r, epaisseur, couleur) {
+    Forme.call(this, epaisseur, couleur) ;
+    this.initX = x ;
+    this.initY = y ;
+    this.rayon = r ;
+
+    this.getInitX = function () {
+        return this.initX;
+    }.bind(this);
+
+    this.getInitY = function () {
+        return this.initY;
+    }.bind(this);
+
+    this.getRayon = function () {
+        return this.rayon;
+    }.bind(this);
+}
+Cercle.prototype = new Forme();
--- view.js
+++ view.js
@@ -1,2 +1,37 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+
+  Forme.prototype.paint = function (ctx) {
+    ctx.beginPath();
+    ctx.lineWidth = this.getEpaisseur();
+    ctx.strokeStyle = this.getCouleur();
+};
+
+Rectangle.prototype.paint = function (ctx) {
+    Forme.prototype.paint.call(this, ctx) ;
+    ctx.rect(this.getInitX(), this.getInitY(), this.getLargeur(), this.getHauteur());
+    ctx.stroke();
+};
+
+Line.prototype.paint = function (ctx) {
+    Forme.prototype.paint.call(this, ctx) ;
+    ctx.moveTo(this.getInitX(), this.getInitY());
+    ctx.lineTo(this.getFinalX(), this.getFinalY());
+    ctx.stroke();
+};
+
+Cercle.prototype.paint = function (ctx) {
+    Forme.prototype.paint.call(this, ctx) ;
+    ctx.arc(this.getInitX(), this.getInitY(), this.getRayon(), 0, 2 * Math.PI);
+    ctx.stroke();
+};
+
+Drawing.prototype.paint = function(ctx) {
+    //console.log(this.getFormes());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.getFormes().forEach(function (eltDuTableau) {
+      // now fill the canvas
+      eltDuTableau.paint(ctx);
+    });
+};  
